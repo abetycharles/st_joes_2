d12a744aeab38af7ba1344c0fe4a288450c28bef
Charles Abety <charlesabety@ubuntu.(none)>
2013-08-14 18:05:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StJoes2::Application.config.secret_token = '3c6edbe453255d51ecae63dc3aef7c8204acba1a549a0beb436c4665e90dc333f11ee45bb95857d6581553db8408375cbb094d02ecf107e46b9b9980c2591b67'
